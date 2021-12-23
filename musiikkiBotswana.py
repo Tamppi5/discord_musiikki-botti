c7f63e96dd2a2834fd4624924c6c6c913c279c2a
Tapio Tuloisela <tapio.tuloisela@aalto.fi>
2021-12-23 19:36:43 +0200

    jotai

--- musiikkiBotswana.py
+++ musiikkiBotswana.py
@@ -1,14 +1,21 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+import yt_dlp
 import os
-import ffmpeg
+import urllib.request
+import re
+import time
+
+def search(search):
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search.replace(" ","_"))
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    return("https://www.youtube.com/watch?v=" + video_ids[0])
 
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix=".")
 
 @client.command()
-async def play(ctx, url : str):
+async def play(ctx, *, haku : str):
     song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
@@ -17,8 +24,10 @@ async def play(ctx, url : str):
         await ctx.send("käytä stop-komentoa")
         return
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name = 'General')
-    await voiceChannel.connect()
+    try:
+        voice_channel = ctx.author.voice.channel
+        await voice_channel.connect()
+    except AttributeError: await ctx.send("Käyttäjä ei ole puhekanavalla")
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
     ydl_opts = {
@@ -29,22 +38,33 @@ async def play(ctx, url : str):
             'preferredquality': '192',
         }],
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(search(haku))
+        await ctx.send(info['duration'])
+        if info['duration'] > 600:
+            return
+        else:
+            ydl.download([search(haku)])
+            for file in os.listdir("./"):
+                if (file.endswith(".mp3")) and not (file.startswith("bababooey")):
+                    await ctx.send("Nyt bängää: " + file.rpartition(".")[0].rpartition("[")[0])
+                    os.rename(file, "song.mp3")
 
-    for file in os.listdir("./"):
-        if file.endswith(".m4a"):
-            os.rename(file, "song.mp3")
-
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+            voice.play(discord.FFmpegPCMAudio('song.mp3'))
 
 @client.command()
 async def leave(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    try: voice.stop()
+    except: print()
+
+    
+    voice.play(discord.FFmpegPCMAudio('bababooey.mp3'))
+    time.sleep(1)
     if voice.is_connected():
         await voice.disconnect()
     else:
-        await ctx.send("Botti ei ole puhekanavalla.")
+        await ctx.send("bruh.")
 
 @client.command()
 async def pause(ctx):
@@ -52,7 +72,7 @@ async def pause(ctx):
     if voice.is_playing():
         voice.pause()
     else:
-        await ctx.send(":smile:")
+        await ctx.send("Ei soi mitään veli")
 
 @client.command()
 async def resume(ctx):
@@ -65,7 +85,7 @@ async def resume(ctx):
 @client.command()
 async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop
+    voice.stop()
 
 
 
